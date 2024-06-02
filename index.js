2d7da8d8969247e26b8b82c780f616af7a84cca8
Martin <martincrovella@gmail.com>
2024-06-02 19:13:58 -0300

    se agregaron los errores

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ Vue.createApp({
             puntaje: null,
             filtroTitulo: '',
             filtroPlataforma: '',
-            filtroEstado: ''
+            filtroEstado: '',
+            errorNombre: '',
+            errorPlataforma: '' ,
+            errorEstado: '',
+            errorPuntaje: '',
+            intentoCargar: false
+          
         };
     },
     computed: {
@@ -19,24 +25,49 @@ Vue.createApp({
                 const coincidePlataforma = this.filtroPlataforma === '' || videojuego.plataforma === this.filtroPlataforma;
                 const coincideEstado = this.filtroEstado === '' || videojuego.estado === this.filtroEstado;
                 return coincideTitulo && coincidePlataforma && coincideEstado;
-            });
+            });            
+        },
+        esNombreInvalido(){
+            return this.nombreJuego.trim() === '' && this.intentoCargar
+            
+        },
+        esPlataformaInvalida(){
+            return this.plataforma === '' && this.intentoCargar
+        },
+        esEstadoinvalido(){
+            return this.estado === "" && this.intentoCargar
+        },
+        esPuntajeInvalido(){
+            return this.puntaje !== null && (this.puntaje < 1 || this.puntaje > 10) && this.intentoCargar
         }
+       
+    
     },
     methods: {
         registrarJuego() {
+            
+            this.intentoCargar = true;
+
+           if(!this.esNombreInvalido && !this.esPlataformaInvalida && !this.esEstadoinvalido && !this.esPuntajeInvalido){
             const nuevoVideojuego = {
                 nombre: this.nombreJuego,
                 plataforma: this.plataforma,
                 estado: this.estado,
                 puntaje: this.puntaje
-            };
-
+            }
             this.listaVideojuegos.push(nuevoVideojuego);
 
             this.nombreJuego = '';
             this.plataforma = '';
             this.estado = '';
             this.puntaje = null;
+                    
+            this.intentoCargar = false;
+           }
+           
+                
+
+            
         },
         mostrarMasInfo(videojuego) {
             this.juegoSeleccionado = videojuego;
